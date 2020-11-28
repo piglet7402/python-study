8d73aaf1284acc1a0098f5657fe23e8d1824bcc3
bsy3764 <60904920+bsy3764@users.noreply.github.com>
2020-11-28 22:55:00 +0900

    modify Photo_Editor1
    코드 수정 Photo_Editor1_v0.1
    저장까지 완료

--- pyqt/image_trans.py
+++ pyqt/image_trans.py
@@ -1,5 +1,6 @@
+import os
 import sys
-from PyQt5.QtWidgets import QFileDialog, QRadioButton
+from PyQt5.QtWidgets import QFileDialog, QGroupBox
 from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5.QtGui import QPixmap, QImage
 from PyQt5.QtCore import Qt
@@ -8,6 +9,7 @@ import numpy as np
 from PyQt5 import uic
 
 form_class = uic.loadUiType("form3.ui")[0]
+counter = 0
 
 class MyApp(QWidget, form_class):
 
@@ -18,12 +20,10 @@ class MyApp(QWidget, form_class):
         self.show()  # 창 보여주기
 
     def initUI(self):
-        self.start = 0
-        self.qimg = 0
+        self.setWindowTitle('Image File')
+        self.move(300, 300)
 
-        # 프로그램 시작시 None 선택된 상태로 시작하기 위해서
-        self.Radio()
-        self.start = 1
+        self.none.setChecked(True)  # none 버튼이 선택되어진 상태로 시작함
 
         self.file.clicked.connect(self.openfile)    # '파일선택' 푸시 버튼이 눌리면 openfile 함수 호출
 
@@ -36,11 +36,8 @@ class MyApp(QWidget, form_class):
         self.gray.stateChanged.connect(self.Check)
         self.invert.stateChanged.connect(self.Check)
 
-        # # 저장 버튼 클릭할 경우
-        # self.save.clicked.connect(self.fileSave)
-
-        self.setWindowTitle('Image File')
-        self.move(300, 300)
+        # 저장 버튼 클릭할 경우
+        self.save.clicked.connect(self.fileSave)
 
     def openfile(self):
         self.fname, _ = QFileDialog.getOpenFileName(self, 'Open file', './')
@@ -62,34 +59,36 @@ class MyApp(QWidget, form_class):
         self.src = cv2.imread(self.fname)  # opencv 이미지 읽어오기
         self.src1 = cv2.cvtColor(self.src, cv2.COLOR_BGR2RGB)
 
-        # 결과 이미지를 Qt 에서 보여주기
-        # self.result_imgShow(self.src_pixmap)
-        # self.result_imgShow(self.src)
-
     def result_imgShow(self, rec_img):
         self.fix_pixmap = rec_img
-        Qimging = QImage(self.fix_pixmap.data, self.fix_pixmap.shape[1], self.fix_pixmap.shape[0], QImage.Format_RGB888)
+
+        # 이미지 pix 이 홀수일 때, 이미지 깨짐을 방지하기 위해
+        width = self.fix_pixmap.shape[1]
+        height = self.fix_pixmap.shape[0]
+        bytesPerLine = width * 3
+
+        Qimging = QImage(self.fix_pixmap.data, width, height, bytesPerLine, QImage.Format_RGB888)
         qimg = QPixmap.fromImage(Qimging)
 
+        qimg_resize = qimg.scaled(self.lbl_W, self.lbl_H, aspectRatioMode=Qt.IgnoreAspectRatio)
+
         # 결과 이미지를 Qt 에서 보여주기
-        self.lbl2.setPixmap(qimg)  # Qt 에서 결과 이미지 보여주기
+        self.lbl2.setPixmap(qimg_resize)  # Qt 에서 결과 이미지 보여주기
 
     def Radio(self):
+        self.result1 = 0
+
         RButton = self.sender()
         if self.blur == RButton:
-            print("blur")
+            # print("blur")
 
             self.result1 = cv2.blur(self.src1, (4, 4))   # blur 효과 주기
-
-            # blurRGB = cv2.cvtColor(self.result1, cv2.COLOR_BGR2RGB)
-            # blurQ= QImage(self.result1.data, self.result1.shape[1], self.result1.shape[0], QImage.Format_RGB888)
-            # qimg = QPixmap.fromImage(blurQ)
 
             # 결과 이미지를 Qt 에서 보여주기
             self.result_imgShow(self.result1)
 
         if self.sharpen == RButton:
-            print("sharpen")
+            # print("sharpen")
             sharpening_2 = np.array([[-1, -1, -1, -1, -1],
                                      [-1, 2, 2, 2, -1],
                                      [-1, 2, 9, 2, -1],
@@ -97,83 +96,85 @@ class MyApp(QWidget, form_class):
                                      [-1, -1, -1, -1, -1]]) / 9.0
             self.result1 = cv2.filter2D(self.src1, -1, sharpening_2)
 
-            # sharpenRGB = cv2.cvtColor(self.result1, cv2.COLOR_BGR2RGB)
-            # sharpenQ = QImage(self.result1.data, self.result1.shape[1], self.result1.shape[0], QImage.Format_RGB888)
-            # qimg = QPixmap.fromImage(sharpenQ)
-
             # 결과 이미지를 Qt 에서 보여주기
             self.result_imgShow(self.result1)
 
         if self.none == RButton:
-            print("none")
+            # print("none")
             self.result1 = self.src1
-            # self.result_imgShow(self.src_pixmap)
-
-            # noneQ = QImage(self.result1.data, self.result1.shape[1], self.result1.shape[0], QImage.Format_RGB888)
-            # qimg = QPixmap.fromImage(noneQ)
 
             # 결과 이미지를 Qt 에서 보여주기
             self.result_imgShow(self.result1)
-
-        if self.start == 0:  # 프로그램이 처음 시작된 상태라면
-            self.none.setChecked(True)  # none 버튼이 선택되어진 상태로 시작함
 
         self.Check()
 
     def Check(self):
 
+        # self.result2 = self.result1
+        # cv2.imshow('after radio', self.result2)
+
         CBox = self.sender()
         # print( type( CBox ) )
-        if type(CBox) == QRadioButton:  # radio button 들어왔을 경우
-            CBox = self.invert
-        if self.invert == CBox:
-            print("invert")
-            self.invertB = self.invert.isChecked()  # invert 박스에 선택됬는지 여부 확인
+        self.invertB = self.invert.isChecked()  # invert 박스에 선택됬는지 여부 확인
+        self.grayB = self.gray.isChecked()  # gray 박스에 선택됬는지 여부 확인
+
+        if self.invert == CBox or self.invertB == True or self.invertB == False:
+            # print("invert")
+
             if self.invertB == True:
-                print("invert : {}".format(self.invertB))
+                # print("invert : {}".format(self.invertB))
                 self.result2 = cv2.bitwise_not(self.result1)
-                # invertQ = QImage(self.result2.data, self.result2.shape[1], self.result2.shape[0], QImage.Format_RGB888)
-                # qimg = QPixmap.fromImage(invertQ)
 
                 # 결과 이미지를 Qt 에서 보여주기
                 self.result_imgShow(self.result2)
 
             if self.invertB == False:
-                print("invert : {}".format(self.invertB))
+                # print("invert : {}".format(self.invertB))
                 # 결과 이미지를 Qt 에서 보여주기
                 self.result2 = self.result1
-                # self.result_imgShow(self.src_pixmap)
-
-                # invertQ = QImage(self.result2.data, self.result2.shape[1], self.result2.shape[0], QImage.Format_RGB888)
-                # qimg = QPixmap.fromImage(invertQ)
 
                 # 결과 이미지를 Qt 에서 보여주기
                 self.result_imgShow(self.result2)
 
-        if self.gray == CBox:
-            print("gray")
-            self.grayB = self.gray.isChecked()  # gray 박스에 선택됬는지 여부 확인
+        if self.gray == CBox or self.grayB == True or self.grayB == False:
+
+            # cv2.imshow('after invert', self.result2)
+            # cv2.waitKey(0)
+            # cv2.destroyAllWindows()
+
+            #print("gray")
+
             if self.grayB == True:
-                print("gray : {}".format(self.grayB))
-                self.result = cv2.cvtColor(self.result2, cv2.COLOR_RGB2GRAY)   # 이미지 색깔 변경하기
-                grqyQ = QImage(self.result.data, self.result.shape[1], self.result.shape[0], QImage.Format_RGB888)
-                qimg = QPixmap.fromImage(grqyQ)
+                # print("gray : {}".format(self.grayB))
+                self.result3 = cv2.cvtColor(self.result2, cv2.COLOR_RGB2GRAY)   # 이미지 색깔 변경하기
+                self.result3 = cv2.cvtColor(self.result3, cv2.COLOR_GRAY2RGB)
 
                 # 결과 이미지를 Qt 에서 보여주기
-                self.result_imgShow(qimg)
+                self.result_imgShow(self.result3)
 
             if self.grayB == False:
-                print("gray : {}".format(self.grayB))
-                self.result_imgShow(self.src_pixmap)
+                # print("gray : {}".format(self.grayB))
+                self.result3 = self.result2
 
-        if CBox == None:
-            print("Not Choice")
+                # 결과 이미지를 Qt 에서 보여주기
+                self.result_imgShow(self.result3)
 
-        print("test!!!!!!")
 
     def fileSave(self):
-        print("save")
-        # 저장 버튼 클릭할 경우
+        # print("save")
+        global counter
+        result_img = cv2.cvtColor(self.result3, cv2.COLOR_RGB2BGR)
+
+        # cv2.imshow('before save', result_img)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+
+        if not os.path.exists('test1'):
+            os.makedirs('test1')
+
+        cv2.imwrite('test1/fix_img.%i'%counter+'.jpg', result_img)
+
+        counter = counter + 1
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
